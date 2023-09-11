5b66b28bcf46a4f0f44bc09d5cd0136009e38692
Andre Santos <andre@dexdev.com.br>
2023-09-11 15:40:17 -0300

    feat: add soma function

---
+++ math.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Soma de 1 + 1 = ", Soma(1, 1))
+}
+
+// Soma retorna a soma de dois inteiros
+func Soma(a, b int) int {
+	return a + b
+}
