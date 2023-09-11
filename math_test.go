99540ed31b7242f30b8aa5b506b3455e8369ea4e
Andre Santos <andre@dexdev.com.br>
2023-09-11 15:59:58 -0300

    fix: unit test

--- math_test.go
+++ math_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSoma(t *testing.T) {
 	total := Soma(15, 15)
-	if total != 30 {
+	if total != 40 {
 		t.Errorf("Soma incorreta, obtido: %d, esperado: %d.", total, 30)
 	}
 }
